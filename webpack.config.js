8975190f2f0569608c8584cd9aa87714d36bf60e
Akshita Kanojia <akshita.kanojia@rapyuta-robotics.com>
2022-07-14 18:57:44 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+function getPlugins() {
+  console.log('\x1b[36m%s\x1b[0m', 'I am cyan');  //cyan
+  console.log('\x1b[33m%s\x1b[0m', 'I am yellow');  //yellow
+  return [];
+}
+
+module.exports = {
+  mode: 'production',
+  entry: './app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'final.js',
+  },
+  target: 'node',
+  plugins: getPlugins()
+};
